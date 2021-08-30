4238496974cc3f486995d7167e1b7f2e877f7364
Enes Polat <37534266+enespolat25@users.noreply.github.com>
2021-08-30 16:19:29 +0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-export const API_KEY = `ba35eb128e4c43778d040f0f47b7a12d`;
+export const API_KEY = `sizin-apiniz`;
 export const endpoint = `https://newsapi.org/v2/top-headlines`;
 export const country = 'tr';
 export const category='general';
